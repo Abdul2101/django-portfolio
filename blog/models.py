5e0e1fa1ca984b1a7bff6fcd7de22ae5190120f3
Abdulkhalyk <itisabdul21@gmail.com>
2024-05-05 22:04:18 +0300

    bootstrap

--- blog/models.py
+++ blog/models.py
@@ -4,3 +4,6 @@ class Blog(models.Model):
     title = models.CharField(max_length=85)
     date = models.DateField()
     text = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.title
